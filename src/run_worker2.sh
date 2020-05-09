4a7cf4e8c0ba9d78de203b9b154eed2653aa47dd
cclausen <colin_clausen@ki.tng.de>
2020-05-09 19:03:38 +0200

    create run_worker2.sh for more automatic worker spawning

---
+++ src/run_worker2.sh
@@ -0,0 +1,86 @@
+#! /bin/bash
+
+# meant to work as root in nvidia/cuda:10.1-cudnn7-devel-ubuntu18.04 docker container
+
+# Use this as the startup script on vast.ai:
+
+# apt install -y wget; wget https://raw.githubusercontent.com/ColaColin/MasterThesis/master/src/run_worker.sh ; chmod +x run_worker.sh; ./run_worker.sh https://x0.cclausen.eu <secret> <runid>
+
+#e.g. ./run_worker.sh https://x0.cclausen.eu <secret> <runid>
+
+apt update
+apt install -y git
+apt install -y gcc
+apt install -y g++
+
+wget https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
+
+chmod +x Anaconda3-2019.10-Linux-x86_64.sh
+
+./Anaconda3-2019.10-Linux-x86_64.sh -b -p /root/anaconda3
+
+export PATH="/root/anaconda3/bin:$PATH"
+eval "$('/root/anaconda3/bin/conda' 'shell.bash' 'hook')"
+
+conda init
+
+conda install -y pytorch torchvision cudatoolkit=10.1 -c pytorch
+
+git clone https://github.com/ColaColin/MasterThesis.git
+cd MasterThesis
+SHA=$(wget -qO- $2/sha/$4)
+git checkout $SHA
+cd src
+
+pip install -r req_workers.txt
+
+chmod +x build.sh
+
+./build.sh
+
+./build.sh testworker
+
+MAX_PER_GPU=3
+
+if nvidia-smi --query-gpu=name --format=csv,noheader | grep -q '1080 Ti'; then
+  MAX_PER_GPU=4
+fi
+
+if nvidia-smi --query-gpu=name --format=csv,noheader | grep -q 'RTX 2070 SUPER'; then
+  MAX_PER_GPU=4
+fi
+
+if nvidia-smi --query-gpu=name --format=csv,noheader | grep -q '2080'; then
+  MAX_PER_GPU=4
+fi
+
+if nvidia-smi --query-gpu=name --format=csv,noheader | grep -q 'RTX 2080 SUPER'; then
+  MAX_PER_GPU=5
+fi
+
+if nvidia-smi --query-gpu=name --format=csv,noheader | grep -q '2080 Ti'; then
+  MAX_PER_GPU=5
+fi
+
+GPUC=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+MAX_BY_GPU=$((MAX_PER_GPU * GPUC))
+
+CPUC=$(nproc)
+MAX_BY_CPU=$((CPUC / 2))
+
+WORKERS=$((MAX_BY_CPU > MAX_BY_GPU ? MAX_BY_GPU : MAX_BY_CPU))
+
+echo $GPUC
+echo $CPUC
+echo $MAX_BY_GPU
+echo $MAX_BY_CPU
+echo $WORKERS
+
+for ((i=1; i <= $WORKERS; i++))
+do
+    python -m core.mains.distributed --command $1 --secret $2 --run $3 --worker $VAST_CONTAINERLABEL --windex $i &>> /root/worker_$i.log &
+done
+
+nvidia-smi -l &>> gpu_load.log &
+
+
